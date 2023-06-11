645fe0534e5bc019a38e09df6294125973c696e2
Amir Mohammad Ghobbeh <amir.ghobbe@gmail.com>
2023-06-12 00:39:57 +0330

    The fifth  edite for responsive web page

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
+const element = document.querySelector('.top_menu');
 function toggle(){
-    const element = document.querySelector('.top_menu');
+   
     
     if(element.style.display == 'flex'){
         element.style.display = 'none';
@@ -9,3 +10,16 @@ function toggle(){
     }
 
 }
+
+
+const mql = window.matchMedia("(min-width: 769px)");
+
+mql.addEventListener("change", (event) => {
+  if (event.matches) {
+    // The viewport is 769 pixels wide or more
+    element.style.display = 'flex';
+  } else {
+    // The viewport is more than 600 pixels wide
+    element.style.display = 'none';
+  }
+});
